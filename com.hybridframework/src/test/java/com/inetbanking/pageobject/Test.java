b03ad81df99b883ec138b6bf05591e8f6dec50b8
venkeyjadhav <104411502+venkeyjadhav@users.noreply.github.com>
2022-04-26 12:04:03 +0530

    Update Test.java

--- com.hybridframework/src/test/java/com/inetbanking/pageobject/Test.java
+++ com.hybridframework/src/test/java/com/inetbanking/pageobject/Test.java
@@ -3,5 +3,10 @@ package com.inetbanking.pageobject;
 public class Test {
     public static void main(String[] args) {
 		System.out.println("main method");
+	        Test t= new Test();
+	        t.test();
+	}
+	public void test(){
+	        System.out.println("test  method");
 	}
 }
